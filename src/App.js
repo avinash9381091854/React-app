9066d3684c32152465094d6642d73234b5d212e5
N Avinash kumar <52166272+avinash9381091854@users.noreply.github.com>
2024-11-12 10:46:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to Hyderabad, Avinash...!!!
+          Edit <code>src/App.js</code> Deploying Nodejs App in Amplify..!!!
         </p>
         <a
           className="App-link"
