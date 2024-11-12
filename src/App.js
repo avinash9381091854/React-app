a5977bbf30f9397d206eb07a5be4d171f6cf4337
N Avinash kumar <52166272+avinash9381091854@users.noreply.github.com>
2024-11-12 10:43:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to Gajuwaka, Visakhapatnam
+          Edit <code>src/App.js</code> Welcome to Hyderabad, Avinash...!!!
         </p>
         <a
           className="App-link"
